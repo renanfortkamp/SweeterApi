660de65d30478c79358ad543049d42e0840c60db
renanfort <renan_fortkamp@estudante.sesisenai.org.br>
2023-01-26 18:58:15 -0300

    AutoMapper Profile

---
+++ Models/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Sweeter.Models.Dto;
+using Sweeter.Models.Entities;
+
+namespace Sweeter.Models.AutoMapper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Sweet, SweetAllDto>().AfterMap((src, dest) =>
+            {
+                dest.DataPostagem = src.DataPostagem.ToString("dd/MM/yyyy HH:mm:ss");
+            });
+
+        }
+    }
+}
