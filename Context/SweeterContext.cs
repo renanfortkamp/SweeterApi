6a9da965e3c0e10369a6090b9aba3fb454ab3884
renanfort <renan_fortkamp@estudante.sesisenai.org.br>
2023-01-26 18:58:32 -0300

    Context

---
+++ Context/SweeterContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sweeter.Models.Entities;
+
+namespace Sweeter.Context
+{
+    public class SweeterContext : DbContext
+    {
+        public SweeterContext(DbContextOptions<SweeterContext> options) : base(options){
+
+        }
+
+        public DbSet<User> users { get; set; }
+        public DbSet<Sweet> sweets { get; set; }
+    }
+}
