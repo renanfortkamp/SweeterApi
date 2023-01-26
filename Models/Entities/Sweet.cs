68811f97d02ffbd56235cfe6a48397e9fa80df13
renanfort <renan_fortkamp@estudante.sesisenai.org.br>
2023-01-26 18:57:38 -0300

    Entidades

---
+++ Models/Entities/Sweet.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sweeter.Models.Entities
+{
+    public class Sweet
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Text { get; set; }
+        public DateTime DataPostagem { get; set; } = DateTime.Now;
+        public int EnumStatus { get; set; } = 1;
+        public bool Editado { get; set; } = false;
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+
+
+
+    }
+}
