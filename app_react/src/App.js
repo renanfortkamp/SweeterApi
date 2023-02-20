4054962c1c6837b1980389d0cd3c974b63942ae0
renanfortkamp <renan_fortkamp@estudante.sesisenai.org.br>
2023-02-20 19:41:11 -0300

    add context para user

--- app_react/src/App.js
+++ app_react/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import RoutesPages from './pages/Routes/RoutesPages.js';
+import Context from './pages/Context/Context.js';
 
 function App() {
   return (
-    <div className="App">
-      <RoutesPages />
-    </div>
+    <Context.Provider value="">
+      <div className="App">
+        <RoutesPages />
+      </div>
+    </Context.Provider>
   );
 }
 
--- app_react/src/pages/Routes/RoutesPages.js
+++ app_react/src/pages/Routes/RoutesPages.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Feed from '../feed/feed.js';
 import Login from '../login/login.js';
@@ -9,10 +9,9 @@ export default function RoutesPages() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" exact element={<Feed />} />
+                <Route path="/" element={<Login />} />
+                <Route path="/register" element={<Register />} />
                 <Route path="/feed" element={<Feed />} />
-                <Route path="/register" element={<Login />} />
-                <Route path="/login" element={<Register />} />
             </Routes>
         </BrowserRouter>
     );
