adae6ca7223380512e36ae5cd7dd97ad4c8d0b9e
renanfortkamp <renan_fortkamp@estudante.sesisenai.org.br>
2023-02-22 00:12:20 -0300

    add logout e retorno dos posts direto no feed

--- app_react/src/pages/feed/feed.js
+++ app_react/src/pages/feed/feed.js
@@ -1,29 +1,36 @@
-import React, { useState,useEffect,useContext } from 'react';
-import Posts from './Posts.js';
+import React, { useState, useEffect, useContext } from 'react';
 import Context from '../Context/Context.js';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Feed() {
     const [text, setText] = useState('');
-    const [posts, setPosts] = useState("");
-    const [context] = useContext(Context);
+    const [posts, setPosts] = useState([]);
+    const [context, setContext] = useContext(Context);
+    const navigate = useNavigate();
 
     const updatePost = () => {
-        setPosts("Atualizando...");
-        setTimeout(() => {
-            setPosts(<Posts />);
-        }, 300);
+        fetch("http://localhost:5066/api/Sweets")
+            .then(e => e.json())
+            .then(data => {
+                setPosts(data);
+            });
+    }
+
+    const Logout = () => {
+        setContext([]);
+        navigate('/');
+
     };
 
     useEffect(() => {
         updatePost();
     }, []);
 
-    console.log(context.name);
-
     const postMessage = (e) => {
         try {
             e.preventDefault();
-            fetch("http://localhost:5066/api/Sweets?userId="+context.id, {
+            fetch("http://localhost:5066/api/Sweets?userId=" + context.id, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
@@ -32,10 +39,10 @@ export default function Feed() {
                     text: text,
                 })
 
-                
+
             })
                 .then(updatePost())
-                
+
 
         } catch (error) {
             alert(error);
@@ -43,17 +50,26 @@ export default function Feed() {
     };
 
     return (
-        <div className='posts' style={{display:'flex',justifyContent:"center",alignItems:"center",flexDirection:"column",border:"solid 1px red"}}>
+        <div className='posts' style={{ display: 'flex', justifyContent: "center", alignItems: "center", flexDirection: "column", border: "solid 1px red" }}>
             <h1>Feed</h1>
             {<p>Bem vindo {context.name}</p>}
-            <form onSubmit={(e)=>{postMessage(e)}}>
+            <form onSubmit={(e) => { postMessage(e) }}>
                 <input type="text" placeholder="text" onChange={(e) => setText(e.target.value)} />
                 <button type="submit">Submit</button>
             </form>
+            <button onClick={() => { Logout() }}>Logout</button>
 
-            {posts}
-            
-            
+            {posts.map(post => (
+                <div key={post.id} className='post' style={{ border: "solid 1px red", width: "400px" }}>
+                    <div>@{post.userName}</div>
+                    <div>{post.text}</div>
+                    <div>{post.dataPostagem}</div>
+                </div>))
+            }
+
+
+
+
 
         </div>
 
